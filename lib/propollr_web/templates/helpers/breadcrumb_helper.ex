584227dbe7031011431ba104e33c9bb806000d80
Nick Stalter <nickstalter@gmail.com>
2018-09-15 12:46:39 -0400

    closes #51. Add icons to breadcrumbs. 🏆

--- lib/propollr_web/templates/helpers/breadcrumb_helper.ex
+++ lib/propollr_web/templates/helpers/breadcrumb_helper.ex
@@ -14,39 +14,50 @@ defmodule PropollrWeb.BreadcrumbHelper do
   end
 
   def crumbs(conn, :dashboard) do
-    [{gettext("Dashboard"), dashboard_path(conn, :index)}]
+    [{gettext("Dashboard"), dashboard_path(conn, :index), "fas fa-home"}]
   end
 
   def crumbs(conn, :sesh, sesh_id) do
-    [{gettext("Sesh"), sesh_path(conn, :view, sesh_id: sesh_id)} | crumbs(conn, :dashboard)]
+    [{gettext("Sesh"), sesh_path(conn, :view, sesh_id: sesh_id), "fas fa-comments"} | crumbs(conn, :dashboard)]
   end
 
   def crumbs(conn, :sesh_edit, sesh_id) do
-    [{gettext("Edit Sesh"), sesh_path(conn, :edit, sesh_id: sesh_id)} | crumbs(conn, :sesh, sesh_id)]
+    [{gettext("Edit Sesh"), sesh_path(conn, :edit, sesh_id: sesh_id), "fas fa-edit"} | crumbs(conn, :sesh, sesh_id)]
   end
 
   def crumbs(conn, :new_question, sesh_id) do
-    [{gettext("New Question"), question_path(conn, :new)} | crumbs(conn, :sesh, sesh_id)]
+    [{gettext("New Question"), question_path(conn, :new), "fas fa-comment-plus"} | crumbs(conn, :sesh, sesh_id)]
   end
 
   def crumbs(conn, :edit_question, sesh_id) do
-    [{gettext("Edit Question"), question_path(conn, :edit)} | crumbs(conn, :sesh_edit, sesh_id)]
+    [{gettext("Edit Question"), question_path(conn, :edit), "fas fa-comment-edit"} | crumbs(conn, :sesh_edit, sesh_id)]
   end
 
   defp render([current | tail]) do
     ([render_crumb(current, :current)] ++ Enum.map(tail, &render_crumb(&1)) |> Enum.reverse())
   end
 
-  defp render_crumb({text, _path}, :current) do
+  defp render_crumb({text, _path, tag}, :current) do
     content_tag :li, class: "is-active" do
-      content_tag :a, do: text
+      [{:safe, breadcrumb_template(text, tag)}]
     end
   end
 
-  defp render_crumb({text, path}) do
+  defp render_crumb({text, path, tag}) do
     content_tag :li do
-      link(text, to: path)
+      [{:safe, breadcrumb_template(text, tag, path)}]
     end
+  end
+
+  defp breadcrumb_template(text, tag, path \\ "#") do
+    """
+      <a href=\"#{path}\">
+        <span class=\"icon\">
+          <i class=\"#{tag}\"></i>
+        </span>
+        <span>#{text}</span>
+      </a>
+    """
   end
 
 end
